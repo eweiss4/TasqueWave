d3e7e25a34be6013276185bec311429573fb3a8c
eweiss4 <148115145+eweiss4@users.noreply.github.com>
2025-02-12 22:40:43 -0600

    Add files via upload

--- forms.py
+++ forms.py
@@ -7,5 +7,5 @@ class TaskForm(FlaskForm):
     description = TextAreaField('Description')
     due_date = DateField('Due Date', format='%Y-%m-%d', validators=[DataRequired()])
     priority = SelectField('Priority', choices=[('Low', 'Low'), ('Medium', 'Medium'), ('High', 'High')], validators=[DataRequired()])
-    tags = StringField('Tags (comma-separated)')
+    tags = StringField('Tags (comma separated, case-sensitive [Exercise,Health])')
     submit = SubmitField('Add Task')
